95485438da2bccd6dff836f257ee6ee63e8b28e0
ShreeThaanu <shree96thaanu@gmail.com>
2022-01-27 10:43:07 +0530

    Initial Commit

---
+++ travelPlanner/travelPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travelPlanner
+//
+//  Created by shree thaanu on 27/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
